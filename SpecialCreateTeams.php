358791745f2debadb65bf8cb830b5c7d89f06694
FO-nTTaX <fo.nttax@yahoo.de>
2017-02-28 13:55:46 +0100

    localize toc heading

--- SpecialCreateTeams.php
+++ SpecialCreateTeams.php
@@ -87,7 +87,7 @@ class SpecialCreateTeams extends SpecialPage
 				'text' => wfMessage( 'createteams-delete-teams-heading' )->inContentLanguage()->text()
 			);
 		}
-		$output->addHTML( '<div id="toc" class="toc"><div id="toctitle"><h2>Contents</h2></div><ul>' );
+		$output->addHTML( '<div id="toc" class="toc"><div id="toctitle"><h2>' . wfMessage( 'toc' )->inContentLanguage()->text() . '</h2></div><ul>' );
 		foreach( $toc as $tocindex => $tocitem ) {
 			$output->addHTML( '<li class="toclevel-1 tocsection-' . ( $tocindex + 1 ) . '"><a href="#' . $tocitem['href'] . '"><span class="tocnumber">' . ( $tocindex + 1 ) . '</span> <span class="toctext">' . $tocitem['text'] . '</span></a></li>' );
 		}
